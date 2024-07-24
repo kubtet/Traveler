7101553bb706e8e2faca5fb96939725fd80da046
Jakub T <kubtet1@wp.pl>
2024-07-24 18:59:28 +0200

    Authentication added

---
+++ API/Helpers/StringFormatHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Helpers;
+
+public class StringFormatHelper
+{
+    public static string MakeFirstCapital(string str)
+    {
+        if (string.IsNullOrEmpty(str))
+        {
+            return string.Empty;
+        }
+
+        char[] letters = str.ToCharArray();
+
+        letters[0] = char.ToUpper(letters[0]);
+
+        return new string(letters);
+    }
+}
