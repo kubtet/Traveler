a4737ed970e44991113b090dd8587f639facc48b
ala0702 <ala07022019@gmail.com>
2024-09-06 00:41:09 +0200

    feat: updated nav-bar and added profile-user component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { ExampleComponent } from './example/example.component';
 import { LoginComponent } from './account/login/login.component';
 import { RegisterComponent } from './account/register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'example', component: ExampleComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'user-profile', component: UserProfileComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
