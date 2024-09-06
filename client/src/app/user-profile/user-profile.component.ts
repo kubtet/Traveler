a91f8ddde68ae58d7beb4d6e10070b5e6c7991f6
ala0702 <ala07022019@gmail.com>
2024-09-06 16:11:44 +0200

    feat: updated ui of user-profile component

--- client/src/app/user-profile/user-profile.component.ts
+++ client/src/app/user-profile/user-profile.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, inject } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { AccountService } from '../services/account.service';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { TabViewModule } from 'primeng/tabview';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    AvatarModule,
+    AvatarGroupModule,
+    CardModule,
+    ButtonModule,
+    TabViewModule,
+  ],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.css'
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent {
-
+  protected accountService = inject(AccountService);
 }
