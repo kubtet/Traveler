227d5437ff7be7ec6a4981c3caf375b71cd53855
Jakub T <kubtet1@wp.pl>
2024-10-19 19:42:55 +0200

    AppInputTextArea component created

---
+++ client/src/app/shared/components/app-input-text-area/app-input-text-area.component.ts
@@ -0,0 +1,55 @@
+import { NgIf } from '@angular/common';
+import { Component, Input, OnChanges } from '@angular/core';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
+@Component({
+  selector: 'app-input-text-area',
+  standalone: true,
+  imports: [InputTextareaModule, NgIf, FormsModule, ReactiveFormsModule],
+  templateUrl: './app-input-text-area.component.html',
+  styleUrl: './app-input-text-area.component.css',
+})
+export class AppInputTextAreaComponent implements OnChanges {
+  /** When present, textarea size changes as being typed. */
+  @Input() public autoResize?: boolean = false;
+
+  /** Number of character columns of the textarea. */
+  @Input() public cols: number = 30;
+
+  /** Used to bind the component to a form control. */
+  @Input() public control?: FormControl<string>;
+
+  /** When present, it specifies that the element value cannot be altered. */
+  @Input() public disabled: boolean = false;
+
+  /** Identifier of the focus input to match a label defined for the component. */
+  @Input() public id: string = '';
+
+  /** Label of the component. */
+  @Input() public label: string = '';
+
+  /** When present, it specifies that an input field must be filled out before submitting the form. */
+  @Input() public required: boolean = false;
+
+  /** Number of character rows of the textarea. */
+  @Input() public rows: number = 5;
+
+  /** Value of the component. */
+  @Input() public value?: string;
+
+  public async ngOnChanges() {
+    if (!this.id) {
+      this.id = this.label;
+    }
+
+    if (this.control) {
+      this.required = this.control.hasValidator(Validators.required);
+    }
+  }
+}
--- client/src/app/travels/travel-add/travel-add.component.ts
+++ client/src/app/travels/travel-add/travel-add.component.ts
@@ -1,7 +1,6 @@
 import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormControl, FormsModule } from '@angular/forms';
-import { DropdownModule } from 'primeng/dropdown';
 import { AppDropdownComponent } from '../../shared/components/app-dropdown/app-dropdown.component';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../models/country';
@@ -9,17 +8,18 @@ import { CityService } from '../../services/city.service';
 import { City } from '../../models/city';
 import { BehaviorSubject } from 'rxjs';
 import { AppLoadingComponent } from '../../shared/components/app-loading/app-loading.component';
+import { AppInputTextAreaComponent } from '../../shared/components/app-input-text-area/app-input-text-area.component';
 
 @Component({
   selector: 'app-travel-add',
   standalone: true,
   imports: [
     AppDropdownComponent,
-    DropdownModule,
-    FormsModule,
-    NgIf,
+    AppInputTextAreaComponent,
     AppLoadingComponent,
     AsyncPipe,
+    FormsModule,
+    NgIf,
   ],
   templateUrl: './travel-add.component.html',
   styleUrl: './travel-add.component.css',
@@ -31,6 +31,7 @@ export class TravelAddComponent implements OnInit {
   protected visitedCity = new FormControl<Country>(undefined);
   protected countries: Country[] = [];
   protected cities: City[] = [];
+  protected travelDescription = new FormControl<string>('');
   protected isLoading = new BehaviorSubject(false);
 
   public async ngOnInit() {
