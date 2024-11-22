449406d6046e0cc02aa73fa0ecb194d9954108b9
Jakub T <kubtet1@wp.pl>
2024-11-23 00:39:41 +0100

    Sending messages on the client implemented

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,5 +1,13 @@
-import { Component, inject, OnInit } from '@angular/core';
 import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  CreateMessageDto,
   MemberDto,
   MessageDto,
   MessagesClient,
@@ -11,6 +19,8 @@ import { AsyncPipe, DatePipe } from '@angular/common';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { AppInputTextComponent } from '../shared/components/app-input-text/app-input-text.component';
 import { AccountService } from '../services/account.service';
+import { AppButtonComponent } from '../shared/components/app-button/app-button.component';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-messages',
@@ -21,23 +31,43 @@ import { AccountService } from '../services/account.service';
     OverlayPanelModule,
     AppInputTextComponent,
     DatePipe,
+    AppButtonComponent,
   ],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css',
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent implements OnInit, AfterViewChecked {
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
   private accountService = inject(AccountService);
   private messageClient = inject(MessagesClient);
   private usersClient = inject(UsersClient);
   protected isLoading = new BehaviorSubject(false);
+  protected isLoadingThread = new BehaviorSubject(false);
   protected threads: MessageDto[] = [];
   protected user: MemberDto = new MemberDto();
+  protected currentThread: BehaviorSubject<MessageDto[]> = new BehaviorSubject<
+    MessageDto[]
+  >([]);
+  protected threadResponderPhotoUrl: BehaviorSubject<string> =
+    new BehaviorSubject<string>('');
+  protected threadResponderUsername: BehaviorSubject<string> =
+    new BehaviorSubject<string>('');
+  protected threadResponderId: BehaviorSubject<number> =
+    new BehaviorSubject<number>(0);
+  protected newMessage: FormControl<string> = new FormControl<string>('');
 
   public async ngOnInit() {
-    await this.loadMessages();
+    await this.loadComponent();
   }
 
-  protected async loadMessages() {
+  public ngAfterViewChecked(): void {
+    if (this.scrollContainer) {
+      this.scrollContainer.nativeElement.scrollTop =
+        this.scrollContainer.nativeElement.scrollHeight;
+    }
+  }
+
+  protected async loadComponent() {
     this.isLoading.next(true);
 
     const user = await firstValueFrom(
@@ -50,15 +80,65 @@ export class MessagesComponent implements OnInit {
       this.user = user;
     }
 
+    this.loadAllThreads();
+    this.isLoading.next(false);
+  }
+
+  protected async loadAllThreads() {
     const messageThreads = await firstValueFrom(
       this.messageClient.getAllMessageThreads()
     );
 
-    if (messageThreads !== null) {
+    if (messageThreads !== null && messageThreads.length !== 0) {
       this.threads = messageThreads;
+
+      messageThreads[0].senderId === this.user.id
+        ? this.loadThread(messageThreads[0].recipientId)
+        : this.loadThread(messageThreads[0].senderId);
+    }
+  }
+
+  protected async loadThread(
+    responderId: number,
+    refreshFlag: boolean = false
+  ) {
+    if (this.threadResponderId.value === responderId && !refreshFlag) {
+      return;
+    }
+    this.isLoadingThread.next(true);
+    const result = await firstValueFrom(
+      this.messageClient.getMessageThread(responderId)
+    );
+
+    this.currentThread.next(result);
+
+    const message = this.currentThread.value[0];
+    if (message.recipientId === this.user.id) {
+      this.threadResponderPhotoUrl.next(message.senderPhotoUrl);
+      this.threadResponderUsername.next(message.senderUsername);
+      this.threadResponderId.next(message.senderId);
+    } else {
+      this.threadResponderPhotoUrl.next(message.recipientPhotoUrl);
+      this.threadResponderUsername.next(message.recipientUsername);
+      this.threadResponderId.next(message.recipientId);
     }
 
-    this.isLoading.next(false);
+    this.isLoadingThread.next(false);
+  }
+
+  protected async sendMessage() {
+    const createMessage: CreateMessageDto = new CreateMessageDto({
+      content: this.newMessage.value,
+      recipientId: this.threadResponderId.value,
+    });
+    const result = await firstValueFrom(
+      this.messageClient.createMessage(createMessage)
+    );
+    if (result) {
+      this.loadThread(this.threadResponderId.value, true);
+      this.loadAllThreads();
+      this.newMessage.setValue('');
+    }
   }
 
   protected isToday(date: Date): boolean {
