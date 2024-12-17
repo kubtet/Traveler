ed654b0b11261a2df6f3b386eb1a648027d7de37
Jakub T <kubtet1@wp.pl>
2024-12-17 13:00:29 +0100

    Adding github workflow and corrections to apiUrl

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   countryStateCityKey:
     'UWVTeklqY21Dc2RhNnE2Q0szM3dtUVV0RnRSQlhhS3JrZUpGM1M0Tg==',
+  apiUrl: 'https://localhost:5001/api/',
   hubsUrl: 'https://localhost:5001/hubs/',
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   countryStateCityKey:
     'UWVTeklqY21Dc2RhNnE2Q0szM3dtUVV0RnRSQlhhS3JrZUpGM1M0Tg==',
+  apiUrl: 'api/',
   hubsUrl: 'hubs/',
 };
