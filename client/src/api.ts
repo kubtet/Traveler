db0705eab2ec83a81c23eb389e39f3a73bf1550a
Jakub T <kubtet1@wp.pl>
2024-12-17 14:12:23 +0100

    api with single slash test

--- client/src/api.ts
+++ client/src/api.ts
@@ -45,7 +45,7 @@ export class AccountClient implements IAccountClient {
     @Optional() @Inject(API_BASE_URL) baseUrl?: string
   ) {
     this.http = http;
-    this.baseUrl = baseUrl;
+    this.baseUrl = baseUrl ?? 'api';
   }
 
   register(registerDto: RegisterDto): Observable<UserDto> {
